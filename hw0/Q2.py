cf012d0ba369632ccc6e723e4b5ed3c9194edfc3
chenchc <bryan830401@gmail.com>
2016-09-23 20:35:09 +0800

    HW0: Fix bugs in image mode.

--- hw0/Q2.py
+++ hw0/Q2.py
@@ -5,7 +5,7 @@ filename = sys.argv[1]
 image = Image.open(filename)
 width, height = image.size
 
-outputImage = Image.new('RGB', (width, height), 'white')
+outputImage = Image.new(image.mode, (width, height), 'white')
 for i in range(0, height, 1):
 	for j in range(0, width, 1):
 		pix = image.getpixel((width - j - 1, height - i - 1))
