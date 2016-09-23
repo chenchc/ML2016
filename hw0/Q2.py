819521f30b6a5ffe769c4c2a5b0f9a6fa7c7f264
chenchc <bryan830401@gmail.com>
2016-09-23 20:28:37 +0800

    HW0: Complete Q2.

---
+++ hw0/Q2.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import sys
+
+filename = sys.argv[1]
+image = Image.open(filename)
+width, height = image.size
+
+outputImage = Image.new('RGB', (width, height), 'white')
+for i in range(0, height, 1):
+	for j in range(0, width, 1):
+		pix = image.getpixel((width - j - 1, height - i - 1))
+		outputImage.putpixel((j, i), pix)
+
+OUTPUT_FILENAME = 'ans2.png'
+outputImage.save(OUTPUT_FILENAME)
