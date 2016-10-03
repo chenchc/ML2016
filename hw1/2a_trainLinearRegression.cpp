8bec54913eed2d82238e3868e04c6a0b3dfc5f55
chenchc <bryan830401@gmail.com>
2016-10-03 22:03:39 +0800

    HW1_LinearRegression: L1 Regulization.

--- hw1/2a_trainLinearRegression.cpp
+++ hw1/2a_trainLinearRegression.cpp
@@ -23,7 +23,7 @@ double train(vector<double> &weight, double &bias, const vector<vector<double> >
     const vector<double> &labelMatrix, int iteration)
 {
     const double ETA = 0.0000000005;
-    const double LAMBDA = 0.0;
+    const double LAMBDA = 0.25;
     double sum_squareError = 0.0;
 
     for (int i = 0; i < iteration; i++) {
@@ -38,7 +38,7 @@ double train(vector<double> &weight, double &bias, const vector<vector<double> >
         // Gradient Descent
         double predictLabel = predict(featureSet, weight, bias);
         for (int i = 0; i < FEATURE_COUNT; i++) {
-            weight[i] += ETA * (2 * (label - predictLabel) * featureSet[i] - 2 * LAMBDA * weight[i]);
+            weight[i] += ETA * (2 * (label - predictLabel) * featureSet[i] - 2 * LAMBDA * (weight[i] > 0.0 ? 1.0 : (weight[i] < 0.0 ? -1.0 : 0.0)));
         }
         bias += ETA * (2 * (label - predictLabel));
 
@@ -109,7 +109,7 @@ int main(int argc, char **argv)
     double lastTestingMSESum = 6e23;
     double testingMSESum = 0.0;
 
-    for (int i = 0; i < 300; i++) {
+    for (int i = 0; i < 400; i++) {
         double mse = train(weight, bias, featureMatrix, labelMatrix, 1000000);
         cout << "Epoch #" << i << ": Training Data MSE=" << mse << endl;
 
